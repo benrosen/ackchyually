a939fd37b0205850fb697a341edad6f1a893bf08
Ben Rosen <benjamin.p.rosen@gmail.com>
2021-12-04 16:52:25 -0500

    create test

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,1 +1,16 @@
-// TODO test
+import assert from "./main";
+
+describe(`${assert.name}`, () => {
+  const assertions = [
+    [0, undefined],
+    [0, undefined],
+    [0, undefined],
+  ] as [number, undefined][];
+  const mock = jest.fn();
+  const name = "";
+
+  assert(assertions, name, mock);
+
+  it("calls the provided function a number of times equal to the number of provided assertions.", () =>
+    expect(mock).toHaveBeenCalledTimes(assertions.length));
+});
